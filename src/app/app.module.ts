a14b9b5fd6c992658fd70ec75b2c744b57a1a984
Rene <rdejramirez@gmail.com>
2021-05-17 20:09:27 -0500

    created component 'crear-producto'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent
   ],
   imports: [
     BrowserModule,
